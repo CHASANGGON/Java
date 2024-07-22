feb3d4a5752aac3ca2fd4dcbdd3f7d1e97d611d3
CHASANGGON <yg9618@naver.com>
2024-07-23 00:00:32 +0900

    4

---
+++ java-start/src/cond/cond1.java
@@ -0,0 +1,14 @@
+package cond;
+
+public class cond1 {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 10) {
+            System.out.println("a는 10보다 크다");
+        } else if (a == 10) {
+            System.out.println("a는 10과 같다");
+        } else {
+            System.out.println("a는 10보다 작다");
+        }
+    }
+}
---
+++ java-start/src/cond/cond3.java
@@ -0,0 +1,10 @@
+package cond;
+
+public class cond3 {
+    public static void main(String[] args) {
+        int age = 10;
+        String status = age >= 18 ? "성인" : "미성년자";
+
+        System.out.println(status);
+    }
+}
