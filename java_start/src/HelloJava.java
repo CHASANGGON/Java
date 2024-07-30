1488c83e3752e84aa8dc467f9608720c93634529
차상곤 <yg9618@naver.com>
2024-07-31 01:30:30 +0900

    Java scanner

--- java_start/src/HelloJava.java
+++ java_start/src/HelloJava.java
@@ -1,5 +1,6 @@
 public class HelloJava { // HelloJava 클래스의 범위 시작
     public static void main(String[] args) { // main 메서드
+
         System.out.println("hello java");
     } // 들여쓰기 (띄어쓰기 4번 = tab)
 } // HelloJava 클래스의 끝
